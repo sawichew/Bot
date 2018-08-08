9f14a90d008b9ae5c7a8050608c929bcefa216d7
Marintas <42211392+Marintas@users.noreply.github.com>
2018-08-08 22:09:28 +0300

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,10 +1,16 @@
-const Discord = require("discord.js");
-const Bot= new Discord.Client();
-client.login(process.env.BOT_TOKEN);
-client.on('message',message=>{
-  if (message.content==='да'){
-    message.reply('нет');
-}
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
 });
 
+client.on('message', message => {
+    if (message.content === 'ping') {
+    	message.reply('pong');
+  	}
+});
 
+// THIS  MUST  BE  THIS  WAY
+client.login(process.env.BOT_TOKEN);
